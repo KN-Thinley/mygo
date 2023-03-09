60d3317964d6deba5d57b83190505d67b5ea159d
KN-Thinley <ryoutamikasa@gmail.com>
2023-03-09 08:36:31 +0000

    this is go fundamentals

---
+++ hello_world/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	mypackage "mygo/myPackage"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	fmt.Println(12)
+	mypackage.Support()
+	helper()
+}
---
+++ myPackage/support.go
@@ -0,0 +1,9 @@
+package mypackage
+
+import (
+	"fmt"
+)
+
+func Support() {
+	fmt.Println("This is my reusable package")
+}
